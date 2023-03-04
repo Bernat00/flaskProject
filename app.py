f4fdd735eae6231aefd3a9a700bc4c4152d498e8
Bernat00 <szabobernat06@gmail.com>
2023-03-04 15:08:52 +0100

    valami

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', foods=['vaami', '100'])
 
 
 if __name__ == '__main__':
