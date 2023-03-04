0eb0046ce471e55b83a6001f1a390ff1666415e1
Bernat00 <szabobernat06@gmail.com>
2023-03-04 14:52:28 +0100

    base

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+<<<<<<< HEAD
 
+=======
+from flask import render_template
+>>>>>>> 8bff3c5 (base)
 app = Flask(__name__)
 
 
 @app.route('/')
+<<<<<<< HEAD
 def hello_world():  # put application's code here
     return 'valami!'
+=======
+def hello_world():
+    return render_template('home.html')
+>>>>>>> 8bff3c5 (base)
 
 
 if __name__ == '__main__':
