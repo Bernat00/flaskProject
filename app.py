de5cb86c9d44e68af32fff208e7c6f7ce9acced7
Bernat00 <szabobernat06@gmail.com>
2023-03-06 22:03:34 +0100

    password

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -9,5 +8,25 @@ def hello_world():
     return render_template('home.html')
 
 
+@app.route('/password', methods=("GET", "POST"))
+def password():
+    return render_template('password.html')
+
+
+def password0():
+    if request.method == "POST":
+        asd = request.form['valami']
+        if asd == 'pass':
+            print('asdasd')
+
+@app.route('/editalas')
+def edit():
+    return render_template('edit.html')
+
+
 if __name__ == '__main__':
     app.run()
+
+
+
+#password
